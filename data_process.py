776ad700e178f7a4378a5d88f36c5eae8d61c34a
Xuanyu Chen <xychen233@gmail.com>
2018-02-17 07:55:55 -0500

    init data pre process

---
+++ data_process.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+a = pd.read_csv("results.csv")
+
+tournament_Set = set()
+team_list_Set = set()
+country_Set = set()
+city_Set = set()
+
+with open("generalData.js","w") as output:
+    output.write("var tournamentList = [" + "\n")
+    for tournament in a['tournament']:
+        if tournament not in tournament_Set:
+            output.write('\"' + tournament + '\",' + "\n")
+            tournament_Set.add(tournament)
+    output.write("];")
+
+    output.write("\n\n")
+
+    output.write("var teamList = [" + "\n")
+    for home_team in a['home_team']:
+        if home_team not in team_list_Set:
+            output.write("\'" + home_team + "\',"+ "\n")
+            team_list_Set.add(home_team)
+    for guest_team in a['away_team']:
+        if guest_team not in team_list_Set:
+            output.write("\'" + guest_team + "\',"+ "\n")
+            team_list_Set.add(guest_team)
+    output.write("];")
+
+    output.write("\n\n")
+
+    output.write("var countryList = [ " + "\n")
+    for country in a['country']:
+        if country not in country_Set:
+            output.write("\'" + country + "\'," + "\n")
+            country_Set.add(country)
+    output.write("];")
+
+    output.write("\n\n")
+
+    output.write("var cityList = [ " + "\n")
+    for city in a['city']:
+        if city not in city_Set:
+            output.write('\"' + city + '\",' + "\n")
+            city_Set.add(city)
+    output.write("];")
