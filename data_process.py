e5c7353d5b67a597a219c184009264968b499b79
Xuanyu Chen <xychen233@gmail.com>
2018-02-20 15:33:42 -0500

    remove space in general data

--- data_process.py
+++ data_process.py
@@ -10,41 +10,41 @@ city_Set = set()
 
 def get_general_data():
     with open("generalData.js","w") as output:
-        output.write("var tournamentList = [" + "\n")
+        output.write("var tournamentList = [")
         for tournament in soccer_data['tournament']:
             if tournament not in tournament_Set:
-                output.write('\"' + tournament + '\",' + "\n")
+                output.write('\"' + tournament + '\",')
                 tournament_Set.add(tournament)
         output.write("];")
 
         output.write("\n\n")
 
-        output.write("var teamList = [" + "\n")
+        output.write("var teamList = [" )
         for home_team in soccer_data['home_team']:
             if home_team not in team_list_Set:
-                output.write("\'" + home_team + "\',"+ "\n")
+                output.write("\'" + home_team + "\',")
                 team_list_Set.add(home_team)
         for guest_team in soccer_data['away_team']:
             if guest_team not in team_list_Set:
-                output.write("\'" + guest_team + "\',"+ "\n")
+                output.write("\'" + guest_team + "\',")
                 team_list_Set.add(guest_team)
         output.write("];")
 
         output.write("\n\n")
 
-        output.write("var countryList = [ " + "\n")
+        output.write("var countryList = [ ")
         for country in soccer_data['country']:
             if country not in country_Set:
-                output.write("\'" + country + "\'," + "\n")
+                output.write("\'" + country + "\',")
                 country_Set.add(country)
         output.write("];")
 
         output.write("\n\n")
 
-        output.write("var cityList = [ " + "\n")
+        output.write("var cityList = [ ")
         for city in soccer_data['city']:
             if city not in city_Set:
-                output.write('\"' + city + '\",' + "\n")
+                output.write('\"' + city + '\",')
                 city_Set.add(city)
         output.write("];")
     output.close()
@@ -108,4 +108,5 @@ def check_country_team():
 
 # check_country_team()
 # get_game_number_by_year()
-get_number_by_tournament()
+# get_number_by_tournament()
+get_general_data()
